ac32babd39865af8fa6a85e254d6f73a7afae51a
Noel Dasco Jr. <79371033+Kiru100@users.noreply.github.com>
2024-05-26 00:18:28 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/images/logo.svg";
 
 const Header = () => {
   return (
-    <header className='bg-gradient-to-tl from-link-water pt-8 px-6 rounded-b-3xl'>
+    <header className='bg-gradient-to-tl from-link-water pt-8 px-6 rounded-b-3xl h-[640px]'>
         <img src={logo} alt="BMI Calculator Logo" className='h-10 w-10 mx-auto'/>
         <h1 className='mt-6 font-Inter-SemiBold text-5xl leading-[110%] text-gunmetal text-center tracking-tighter' >Body Mass Index Calculator</h1>
         <p className="text-dark-electric-blue font-Inter-Normal leading-[150%] text-base mt-6">Better understand your weight in relation to your height using our bodyF mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
